f1ca677b7349c5b2222aa9502a968b2e82b412ac
Jcupzz <jcupzz255@gmail.com>
2021-06-15 11:43:38 +0530

    FIXED ADDED VALUES

--- microirrigation/lib/data.dart
+++ microirrigation/lib/data.dart
@@ -8,6 +8,7 @@ class Data {
     double _3 = sharedPreferences.getDouble("_3");
     double _4 = sharedPreferences.getDouble("_4");
     double _5 = sharedPreferences.getDouble("_5");
+
     double _7 = sharedPreferences.getDouble("_7");
     double _8 = sharedPreferences.getDouble("_8");
     double _9 = sharedPreferences.getDouble("_9");
@@ -24,10 +25,27 @@ class Data {
     double _20 = sharedPreferences.getDouble("_20");
     double _21 = sharedPreferences.getDouble("_21");
 
+    double _23 = sharedPreferences.getDouble("_23");
+    double _24 = sharedPreferences.getDouble("_24");
+    double _25 = sharedPreferences.getDouble("_25");
+    double _26 = sharedPreferences.getDouble("_26");
+    double _27 = sharedPreferences.getDouble("_27");
+    double _28 = sharedPreferences.getDouble("_28");
+
     //Equation Starts here
     double _d2 = _2 * _3;
+    double _d3 = _7 * _8;
+    double _d4 = _2 / _7;
+    double _d5 = _3 / _8;
+    double _d6 = _d4 * _d5;
 
     double _d16 = _2 / 2;
+    double _d18 = _d16 * 1;
+    double _d19 = _3 - (_8 / 2);
+    double _d21 = _d19 * 1;
+    double _d22 = _2 - (_7 / 2);
+    double _d24 = _d5 * 2;
+    double _d25 = _d24 * _d22;
 
     print(_d16);
     print("REACHEFJKJFKFDKFDKF");
